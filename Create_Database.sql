0911ca6da515f6229cbc781780356a676ae7e332
Nguyen Cong Vinh <80946737+ncv2k@users.noreply.github.com>
2021-03-19 17:05:50 +0700

    Initial commit

---
+++ Create_Database.sql
@@ -0,0 +1,188 @@
+--   __   __  _                    _        __               
+--   \ \ / / (_)                  | |       \_\              
+--    \ V /   _   _ __       ___  | |__     __ _    ___      
+--     > <   | | | '_ \     / __| | '_ \   / _` |  / _ \     
+--    / . \  | | | | | |   | (__  | | | | | (_| | | (_) |    
+--   /_/_\_\ |_| |_| |_|    \___| |_| |_|  \__,_|  \___/ 
+--
+--    _____                    __      __              _
+--   |_   _| ( )               \ \    / / (_)         | |    
+--     | |   |/   _ __ ___      \ \  / /   _   _ __   | |__  
+--     | |       | '_ ` _ \      \ \/ /   | | | '_ \  | '_ \ 
+--    _| |_      | | | | | |      \  /    | | | | | | | | | |
+--   |_____|     |_| |_| |_|       \/     |_| |_| |_| |_| |_|
+--                                          
+-- Contact: vn.ncvinh@gmail.com                                         
+-- Facebook: facebook.com/vn.ncvinh
+-- Github: github.com/vnncv
+
+
+
+
+
+
+
+/* Drop the old database (if exists)
+
+use master
+drop database Airplane_Manage
+
+*/
+
+
+-- Create database and Table
+
+create database Airplane_Manage
+go
+
+use Airplane_Manage
+go
+
+create table CHUYENBAY (
+MaCB char(5) primary key,
+GaDi varchar(3),
+GaDen varchar(3),
+DoDai int,
+GioDi time,
+GioDen time,
+ChiPhi int)
+go
+
+create table MAYBAY(
+MaMB int primary key,
+Loai varchar(30),
+TamBay int)
+go
+
+create table NHANVIEN(
+MaNV int primary key,
+Ten varchar(30),
+Luong int)
+go
+
+create table CHUNGNHAN(
+MaNV int references NHANVIEN(MaNV),
+MaMB int references MAYBAY(MaMB),
+constraint pk primary key (MaNV, MaMB))
+go
+
+-- Insert data to table CHUYENBAY
+
+Insert CHUYENBAY
+values
+('VN431','SGN','CAH',3693,'5:55','6:55',236),
+('VN320','SGN','DAD',2798,'6:00','7:10',221),
+('VN464','SGN','DLI',2002,'7:20','8:05',225),
+('VN216','SGN','DIN',4170,'10:30','14:20',262),
+('VN280','SGN','HPH',11979,'6:00','8:00',1279),
+('VN254','SGN','HUI',8765,'18:40','20:00',781),
+('VN338','SGN','BMV',4081,'15:25','16:25',375),
+('VN440','SGN','BMV',4081,'18:30','19:30',426),
+('VN651','DAD','SGN',2798,'19:30','8:00',221),
+('VN276','DAD','CXR',1283,'9:00','12:00',203),
+('VN374','HAN','VII',510,'11:40','13:25',120),
+('VN375','VII','CXR',752,'14:15','16:00',181),
+('VN269','HAN','CXR',1262,'14:10','15:50',202),
+('VN315','HAN','DAD',134,'11:45','13:00',112),
+('VN317','HAN','UIH',827,'15:00','16:15',190),
+('VN741','HAN','PXU',395,'6:30','8:30',120),
+('VN474','PXU','PQC',1586,'8:40','11:20',102),
+('VN476','UIH','PQC',485,'9:15','11:50',117)
+go
+
+-- Insert data to table MAYBAY
+
+Insert MAYBAY
+values
+(747,'Boeing 747 - 400',13488),
+(737,'Boeing 737 - 800',5413),
+(340,'Airbus A340 - 300',11392),
+(757,'Boeing 757 - 300',6416),
+(777,'Boeing 777 - 300',10306),
+(767,'Boeing 767 - 400ER',10360),
+(320,'Airbus A320',4168),
+(319,'Airbus A319',2888),
+(727,'Boeing 727',2406),
+(154,'Tupolev 154',6565)
+go
+
+-- Insert data to table NHANVIEN
+
+Insert NHANVIEN
+values
+(242518965,'Tran Van Son',120433),
+(141582651,'Doan Thi Mai',178345),
+(11564812,'Ton Van Quy',153972),
+(567354612,'Quan Cam Ly',256481),
+(552455318,'La Que',101745),
+(550156548,'Nguyen Thi Cam',205187),
+(390487451,'Le Van Luat',212156),
+(274878974,'Mai Quoc Minh',99890),
+(254099823,'Nguyen Thi Quynh',24450),
+(356187925,'Nguyen Vinh Bao',44740),
+(355548984,'Tran Thi Thai An',212156),
+(310454876,'Ta Van Do',212156),
+(489456522,'Nguyen Thi Quy Linh',127984),
+(489221823,'Bui Quoc Chinh',23980),
+(548977562,'Le Van Quy',134476),
+(310454877,'Tran Van Mao',33546),
+(142519864,'Nguyen Thi Xuan Dao',227489),
+(269734834,'Truong Tuan Anh',289950),
+(287321212,'Duong Van Minh',48090),
+(552455348,'Bui Thi Dung',92013),
+(248965255,'Iran Thi 13a',43723),
+(159542516,'Le Van Ky',48250),
+(348121549,'Nguyen Van Thanh',32899),
+(574489457,'Hui Van Lap',20)
+go
+
+
+-- Insert data to table CHUNGNHAN
+Insert CHUNGNHAN
+values
+(567354612,747),
+(567354612,737),
+(567354612,757),
+(567354612,777),
+(567354612,767),
+(567354612,727),
+(567354612,340),
+(552455318,737),
+(552455318,319),
+(552455318,747),
+(552455318,767),
+(390487451,340),
+(390487451,320),
+(390487451,319),
+(274878974,757),
+(274878974,767),
+(355548984,154),
+(310454876,154),
+(142519864,747),
+(142519864,757),
+(142519864,777),
+(142519864,767),
+(142519864,737),
+(142519864,340),
+(142519864,320),
+(269734834,747),
+(269734834,737),
+(269734834,340),
+(269734834,757),
+(269734834,777),
+(269734834,767),
+(269734834,320),
+(269734834,319),
+(269734834,727),
+(269734834,154),
+(242518965,737),
+(242518965,757),
+(141582651,737),
+(141582651,757),
+(141582651,767),
+(11564812,737),
+(11564812,757),
+(574489457,154)
+go
+
+-- End Create and Insert Database
---
+++ TH 4_5.sql
@@ -0,0 +1,364 @@
+﻿--   __   __  _                    _        __               
+--   \ \ / / (_)                  | |       \_\              
+--    \ V /   _   _ __       ___  | |__     __ _    ___      
+--     > <   | | | '_ \     / __| | '_ \   / _` |  / _ \     
+--    / . \  | | | | | |   | (__  | | | | | (_| | | (_) |    
+--   /_/_\_\ |_| |_| |_|    \___| |_| |_|  \__,_|  \___/ 
+--
+--    _____                    __      __              _
+--   |_   _| ( )               \ \    / / (_)         | |    
+--     | |   |/   _ __ ___      \ \  / /   _   _ __   | |__  
+--     | |       | '_ ` _ \      \ \/ /   | | | '_ \  | '_ \ 
+--    _| |_      | | | | | |      \  /    | | | | | | | | | |
+--   |_____|     |_| |_| |_|       \/     |_| |_| |_| |_| |_|
+--                                          
+-- Contact: vn.ncvinh@gmail.com                                         
+-- Facebook: facebook.com/vn.ncvinh
+-- Github: github.com/vnncv
+
+
+
+
+
+-- Bài 1. Cho biết thông tin về các chuyến bat đi Đà Lạt (DAD) 
+	use Airplane_Manage
+	select *
+	from CHUYENBAY
+	where GaDi='DAD'
+	
+-- Bài 2. Cho biết thông tin về các loại máy bay có tầm bay lớn hơn 10.000 km. 
+	use Airplane_Manage
+	select *
+	From MAYBAY
+	where TamBay > 10000
+
+-- Bài 3. Cho biết thông tin về các nhân viên có lương nhỏ hơn 10000 
+	use Airplane_Manage
+	select *
+	from NHANVIEN
+	where Luong<10000
+
+-- Bài 4. Cho biết thông tin về các chuyến bay có độ dài đường bay nhỏ hơn 10000km và lớn hơn 8000km 
+	use Airplane_Manage
+	select *
+	from CHUYENBAY
+	where DoDai between 8000 and 10000
+
+-- Bài 5. Cho biết thông tin về các chuyến bay xuất phát từ Sài Gòn (SGN) đi Ban Mê Thuột (BMV) 
+	use Airplane_Manage
+	select *
+	from CHUYENBAY
+	where GaDi='SGN' and GaDen='BMV'
+
+-- Bài 6. Có bao nhiêu chuyến bay xuất phát từ Sài Gòn (SGN) 
+	use Airplane_Manage
+	select count(MaCB) as N'Số chuyến bay từ Sài Gòn'
+	from CHUYENBAY
+	where GaDi='SGN'
+
+-- Bài 7. Có bao nhiêu loại máy bay Boeing 
+	use Airplane_Manage
+	select count(MaMB) as N'Số Loại Máy Bay Boeing'
+	From MAYBAY
+	where Loai like 'Boeing %'
+
+-- Bài 8. Cho biết tổng số lương phải trả cho các nhân viên 
+	use Airplane_Manage
+	select sum(Luong) as N'Tổng Lương'
+	from NHANVIEN
+
+-- Bài 9. Cho biết mã số và tên của các phi công lái máy bay Boeing 
+	use Airplane_Manage
+	select NHANVIEN.MaNV as 'Mã NV', NHANVIEN.Ten As 'Tên'
+	From NHANVIEN, CHUNGNHAN, MAYBAY
+	where NHANVIEN.MaNV=CHUNGNHAN.MaNV and CHUNGNHAN.MaMB=MAYBAY.MaMB and MAYBAY.Loai like 'Boeing %'
+	group by NHANVIEN.MaNV, NHANVIEN.Ten
+
+-- Bài 10. Cho biết mã số và tên của các phi công có thể lái được máy bay có mã số là 747 
+	use Airplane_Manage
+	select NHANVIEN.MaNV as 'Mã NV', NHANVIEN.Ten As 'Tên'
+	From NHANVIEN, CHUNGNHAN
+	where NHANVIEN.MaNV=CHUNGNHAN.MaNV and CHUNGNHAN.MaMB=747
+
+-- Bài 11. Co biết mã số của các loại máy bay mà nhân viên có họ Nguyễn có thể lái 
+	use Airplane_Manage
+	select CHUNGNHAN.MaMB as N'Mã MB'
+	from NHANVIEN, CHUNGNHAN
+	where NHANVIEN.MaNV = CHUNGNHAN.MaNV and NHANVIEN.Ten like N'Nguyen %'
+
+-- Bài 12. Cho biết mã số của các phi công vừa lái được Boeing vừa lại được Airbus A320 
+	use Airplane_Manage
+	select CHUNGNHAN.MaNV as N'Mã NV'
+	From CHUNGNHAN, MAYBAY
+	where CHUNGNHAN.MaMB=MAYBAY.MaMB and (MAYBAY.Loai like 'Boeing %' or MAYBAY.Loai = 'Airbus A320')
+	group by CHUNGNHAN.MaNV
+
+-- Bài 13. Cho biết các loại máy bay có thể thực hiện được chuyến bay VN280 
+	use Airplane_Manage
+	Select Loai
+	From MAYBAY
+	where TamBay>=(select DoDai from CHUYENBAY where MaCB='VN280')
+	group by Loai
+
+-- Bài 14. Cho biết các chuyến bay có thể thực hiện bởi máy bay Airbus A320 
+	use Airplane_Manage
+	Select MaCB
+	from CHUYENBAY
+	where DoDai <= (select TamBay from MAYBAY where Loai='Airbus A320')
+	group by MaCB
+
+-- Bài 15. Cho biết tên của các phi công lái máy bay Boeing 
+	use Airplane_Manage
+	select NHANVIEN.Ten as N'Nhân Viên Có thể lái Boeing'
+	from NHANVIEN, CHUNGNHAN, MAYBAY
+	where NHANVIEN.MaNV=CHUNGNHAN.MaNV and CHUNGNHAN.MaMB=MAYBAY.MaMB and MAYBAY.Loai like 'Boeing %'
+	group by NHANVIEN.Ten
+
+-- Bài 16. Với mỗi loại máy bay có phi công lái, cho biết mã số, loại máy bay và tổng số phi công có thể lái loại máy bay đó 
+	use Airplane_Manage
+	Select MAYBAY.MaMB As N'Mã Số', MAYBAY.Loai As N'Loại MB', count (NHANVIEN.MaNV) as N'Số Phi Công Có thể lái'
+	from MAYBAY, NHANVIEN, CHUNGNHAN
+	where MAYBAY.MaMB=CHUNGNHAN.MaMB and CHUNGNHAN.MaNV=NHANVIEN.MaNV
+	group by MAYBAY.MaMB, MAYBAY.Loai
+
+-- Bài 17. Giả sử một hành khách muốn đi thẳng từ ga A đến ga B rồi quay trở về ga A. Cho biết các đường bay nào có thể đáp ứng yêu cầu này. 
+	
+
+-- Bài 18. Với mỗi ga có chuyến bay xuất phát từ đó, cho biết có bao nhiêu chuyến bay khởi hành từ ga đó 
+	use Airplane_Manage
+	select gadi, count(gadi) as 'So chuyen bay'
+	from chuyenbay
+	group by GaDi
+
+-- Bài 19. Với mỗi ga có chuyến bay xuất phát từ đó, cho biết tổng chi phí phải trả chi phi công lái các chuyến bay khởi hành từ ga đó. 
+	use Airplane_Manage
+	select GaDi, sum(ChiPhi) as 'Tong Chi Phi't
+	from CHUYENBAY
+	group by GaDi
+
+-- Bài 20. Với mỗi ga xuất phát, cho biết có bao nhiêu chuyến bay có thể khởi hành trước 12:00  
+	use Airplane_Manage
+	select GaDi, count(MaCB) as 'Tong CB'
+	from CHUYENBAY
+	where GioDi < '12:00:00'
+	group by GaDi
+
+-- Bài 21. Cho biết mã số của phi công chỉ lái được 3 loại máy bay 
+	use Airplane_Manage
+	select NHANVIEN.MaNV
+	from NHANVIEN, CHUNGNHAN
+	where NHANVIEN.MaNV=CHUNGNHAN.MaNV
+	group by NHANVIEN.MaNV
+	HAVING count(CHUNGNHAN.MaMB) = 3
+
+-- Bài 22. Với mỗi phi công có thể lái nhiều hơn 3 loại máy bay, cho biết mã số phi công và tầm bay lớn nhất của các loại máy bay mà phi công đó có thể lái 
+	use Airplane_Manage
+	select NHANVIEN.MaNV, NHANVIEN.Ten, MAX(MAYBAY.TamBay) as 'Tam Bay Lon Nhat'
+	from NHANVIEN, CHUNGNHAN, MAYBAY
+	where NHANVIEN.MaNV=CHUNGNHAN.MaNV and CHUNGNHAN.MaMB=MAYBAY.MaMB
+	group by NHANVIEN.MaNV, NHANVIEN.Ten
+	HAVING count(CHUNGNHAN.MaMB) > 3
+
+-- Bài 23. Cho biết mã số của các phi công có thể lái được nhiều loại máy bay nhất 
+	use Airplane_Manage
+	select MaNV
+	from CHUNGNHAN
+	group by MaNV
+	having count(MaMB) >= All(select count(MaNV) from CHUNGNHAN group by MaNV)
+
+-- Bài 24. Cho biết mã số của các phi công có thể lái được ít loại máy bay nhất. 
+	use Airplane_Manage
+	select MaNV
+	from CHUNGNHAN
+	group by MaNV
+	having count(MaMB) <= All(select count(MaNV) from CHUNGNHAN group by MaNV)
+
+-- Bài 25. Tìm các nhân viên không phải là phi công 
+	use Airplane_Manage
+	select NHANVIEN.MaNV, NHANVIEN.Ten, NHANVIEN.Luong
+	from CHUNGNHAN right join NHANVIEN
+	on CHUNGNHAN.MaNV=NHANVIEN.MaNV
+	group by NHANVIEN.MaNV, NHANVIEN.Ten, NHANVIEN.Luong
+	having count(CHUNGNHAN.MaMB) = 0
+
+-- Bài 26. Cho biết mã số của các nhân viên có lương cao nhất 
+	use Airplane_Manage
+	select MaNV, Luong
+	from NHANVIEN
+	group by MaNV, Luong
+	having Luong = (select Max(Luong) from NHANVIEN)
+
+-- Bài 27. Cho biết tổng số lương phải trả cho các phi công 
+	use Airplane_Manage
+	select sum(Luong) as 'Tong luong'
+	from NHANVIEN
+
+-- Bài 28. Tìm các chuyến bay có thể được thực hiện bởi tất cả các loại máy bay Boeing 
+	use Airplane_Manage
+	select MaCB
+	from CHUYENBAY
+	group by MaCB, DoDai
+	having DoDai <= (select min(TamBay) from MAYBAY where Loai like 'Boeing %')
+
+-- Bài 29. Cho biết mã số của các máy bay có thể được sử dụng để thực hiện chuyến bay từ Sài gòn (SGN) đến Huế (HUI) 
+	use Airplane_Manage
+	select MaMB, Loai
+	from MAYBAY
+	group by MaMB,Loai, TamBay
+	having TamBay >= (select DoDai from CHUYENBAY where GaDi='SGN' and GaDen='HUI')
+
+-- Bài 30. Tìm các chuyến bay có thể được lái bởi các phi công có lương lớn hơn 100.000 
+	use Airplane_Manage
+	select MaCB, GaDi, GaDen
+	from CHUYENBAY
+	group by MaCB, GaDi, GaDen, DoDai
+	having DoDai<=All(select MAYBAY.TamBay from MAYBAY,NHANVIEN,CHUNGNHAN where NHANVIEN.MaNV=CHUNGNHAN.MaNV and CHUNGNHAN.MaMB=MAYBAY.MaMB and NHANVIEN.Luong>100000)
+	
+-- Bài 31. Cho biết tên các phi công có lương nhỏ hơn chi phí thấp nhất của đường bay từ Sài Gòn (SGN) đến Buôn mê Thuột (BMV) 
+	use Airplane_Manage
+	select NHANVIEN.Ten
+	from NHANVIEN, CHUNGNHAN, MAYBAY
+	where NHANVIEN.MaNV=CHUNGNHAN.MaNV and CHUNGNHAN.MaMB=MAYBAY.MaMB
+	group by NHANVIEN.Ten, NHANVIEN.Luong, MAYBAY.TamBay
+	having NHANVIEN.Luong < (select min(CHUYENBAY.ChiPhi) from CHUYENBAY where GaDi='SGN' and GaDen='BMV')
+
+-- Bài 32. Cho biết mã số của các phi công có lương cao nhất 
+	use Airplane_Manage
+	select MaNV, Luong
+	from NHANVIEN
+	group by MaNV, Luong
+	having Luong = (select Max(Luong) from NHANVIEN)
+
+-- Bài 33. Cho biết mã số của các nhân viên có lương cao thứ nhì 
+	use Airplane_Manage
+	select MaNV, Luong
+	from NHANVIEN
+	group by MaNV, Luong
+	having Luong = (select Max(Luong) from NHANVIEN where luong < (select Max(Luong) from NHANVIEN))
+
+
+-- Bài 34. Cho biết mã số của các phi công có lương cao nhất hoặc thứ nhì 
+	use Airplane_Manage
+	select MaNV, Luong
+	from NHANVIEN
+	group by MaNV, Luong
+	having Luong >= (select Max(Luong) from NHANVIEN where luong < (select Max(Luong) from NHANVIEN))
+
+
+-- Bài 35. Cho biết tên và lương của các nhân viên không phải là phi công và có lương lớn hơn lương trung bình của tất cả các phi công. 
+	use Airplane_Manage
+	select NHANVIEN.Ten, NHANVIEN.Luong
+	from CHUNGNHAN right join NHANVIEN
+	on CHUNGNHAN.MaNV=NHANVIEN.MaNV
+	group by NHANVIEN.Ten, NHANVIEN.Luong
+	having count(CHUNGNHAN.MaMB) = 0 and NHANVIEN.Luong > (select avg(Luong) from NHANVIEN where MaNV in (select NHANVIEN.MaNV from NHANVIEN, CHUNGNHAN where NHANVIEN.MaNV=CHUNGNHAN.MaNV group by NHANVIEN.MaNV))
+
+-- Bài 36. Cho biết tên các phi công có thể lái các máy bay có tầm bay lớn hơn 4.800km nhưng không có chứng nhận lái máy bay Boeing 
+	use Airplane_Manage
+	select NHANVIEN.Ten
+	from NHANVIEN, CHUNGNHAN, MAYBAY
+	where NHANVIEN.MaNV=CHUNGNHAN.MaNV and CHUNGNHAN.MaMB=MAYBAY.MaMB and MAYBAY.Loai not like 'Boeing%' and MAYBAY.TamBay>4800
+	group by NHANVIEN.Ten
+
+-- Bài 37. Cho biết tên các phi công lái ít nhất 3 loại máy bay có tầm xa hơn 3200km 
+	use Airplane_Manage
+	select NHANVIEN.Ten
+	from NHANVIEN, CHUNGNHAN, MAYBAY
+	where NHANVIEN.MaNV=CHUNGNHAN.MaNV and CHUNGNHAN.MaMB=MAYBAY.MaMB and MAYBAY.TamBay>3200
+	group by NHANVIEN.Ten
+	having count(MAYBAY.MaMB) >= 3
+
+-- Bài 38. Với mỗi nhân viên cho biết mã số, tên nhân viên và tổng số loại máy bay mà nhân viên đó có thể lái 
+	use Airplane_Manage
+	select NHANVIEN.MaNV, NHANVIEN.Ten, count(CHUNGNHAN.MAMB) as 'Tong may bay co the lai'
+	from NHANVIEN left join CHUNGNHAN
+	on NHANVIEN.MaNV=CHUNGNHAN.MaNV
+	group by NHANVIEN.MaNV, NHANVIEN.Ten
+
+-- Bài 39. Với mỗi nhân viên, cho biết mã số, tên nhân viên và tổng số loại máy bay Boeing mà nhân viên đó có thể lái 
+	use Airplane_Manage
+	select NHANVIEN.MaNV, NHANVIEN.Ten, count(CHUNGNHAN.MAMB) as 'Tong may bay co the lai'
+	from NHANVIEN left join CHUNGNHAN
+	on NHANVIEN.MaNV=CHUNGNHAN.MaNV left join MAYBAY on CHUNGNHAN.MaMB=MAYBAY.MaMB and MAYBAY.Loai like 'Boeing%'
+	group by NHANVIEN.MaNV, NHANVIEN.Ten
+
+-- Bài 40. Với mỗi loại máy bay,  cho biết loại máy bay và tổng số phi công có thể lái loại máy bay đó. 
+	use Airplane_Manage
+	select MAYBAY.MaMB, MAYBAY.Loai, Count(CHUNGNHAN.MaNV) as 'So Phi Cong Co The Lai'
+	from MAYBAY left join CHUNGNHAN
+	on MAYBAY.MaMB=CHUNGNHAN.MaMB
+	Group by MAYBAY.MaMB, MAYBAY.Loai
+
+-- Bài 41. Với mỗi loại máy bay, cho biết loại máy bay và tổng số chuyến bay không thể thực hiện bởi loại máy bay đó. 
+	use Airplane_Manage
+	select MAYBAY.Loai, COUNT(CHUYENBAY.MaCB) as 'So Chuyen Khong The Thuc Hien'
+	from MAYBAY left join CHUYENBAY
+	on MAYBAY.TamBay<CHUYENBAY.DoDai
+	group by MAYBAY.Loai
+
+-- Bài 42. Với mỗi loại máy bay, hãy cho biết loại máy bay và tổng số phi công có lương lớn hơn 100.000 có thể lái loại máy bay đó. 
+	use Airplane_Manage
+	select MAYBAY.Loai,count(NHANVIEN.MaNV)
+	from MAYBAY left join CHUNGNHAN on MAYBAY.MaMB=CHUNGNHAN.MaMB left join NHANVIEN on CHUNGNHAN.MaNV = NHANVIEN.MaNV and NHANVIEN.Luong>100000
+	group by MAYBAY.Loai
+
+-- Bài 43. Với mỗi loại máy bay có tầm bay trên 3200km, cho biết tên của loại máy bay và lương trung bình của các phi công có thể lái loại máy bay đó. 
+	use Airplane_Manage
+	select NHANVIEN.MaNV
+	from MAYBAY, CHUNGNHAN, NHANVIEN
+	where MAYBAY.MaMB=CHUNGNHAN.MaMB and CHUNGNHAN.MaNV=NHANVIEN.MaNV and MAYBAY.TamBay>3200
+	group by MAYBAY.Loai
+
+-- Bài 44. Với mỗi loại máy bay  hãy cho biết loại máy bay và tổng số nhân viên không thể lái loại máy bay đó 
+	use Airplane_Manage
+	select MAYBAY.MaMB, (select count(MaNV) from NHANVIEN)- count(CHUNGNHAN.MaNV) as 'So NV khong the lai'
+	from CHUNGNHAN, MAYBAY
+	where CHUNGNHAN.MaMB=MAYBAY.MaMB
+	group by MAYBAY.MaMB
+
+-- Bài 45. Với mỗi loại máy bay  hãy cho biết loại máy bay và tổng số phi công không thể lái loại máy bay đó. 
+	use Airplane_Manage
+	select MAYBAY.MaMB, (select count(manv) from NHANVIEN where MaNV in (select NHANVIEN.MaNV from NHANVIEN, CHUNGNHAN where NHANVIEN.MaNV=CHUNGNHAN.MaNV group by NHANVIEN.MaNV))- count(CHUNGNHAN.MaNV) as 'So NV khong the lai'
+	from CHUNGNHAN, MAYBAY
+	where CHUNGNHAN.MaMB=MAYBAY.MaMB
+	group by MAYBAY.MaMB
+
+
+-- Bài 46. Với mỗi nhân viên, cho biết mã số, tên nhân viên và tổng số chuyến bay xuất phát từ Sài Gòn mà nhân viên đó không thể lái
+	
+
+-- Bài 47. Với mỗi phi công, cho biết mã số, tên phi công và tổng số chuyến bay xuất phát từ Sài Gòn mà phi công đó có thể lái 
+
+-- Bài 48. Với mỗi chuyến bay, hãy cho biết mã số chuyến bay và tổng số loại máy bay có thể thực hiện chuyến bay đó 
+	use Airplane_Manage
+	select CHUYENBAY.MaCB, count(MAYBAY.MaMB)
+	from CHUYENBAY, MAYBAY
+	where CHUYENBAY.DoDai < MAYBAY.TamBay
+	group by CHUYENBAY.MaCB
+
+-- Bài 49. Với mỗi chuyến bay, cho biết mã số chuyến bay và tổng số phi công không thể lái chuyến đó. 
+	
+-- Bài 50. Một hành khách muốn đi từ Hà Nội (HAN) đến nha trang (CXR) mà không phải đổi chuyến bay quá một lần. Cho biết mã chuyếnbay, thời gian khời hành từ Hà nội nếu hành khách muốn đến Nha Trang trước 16:00 
+	
+-- Bài 51. Cho biết thông tin của các đường bay mà tất cả các phi công có thể bay trên đường bay đó đều có lương lớn hơn 100000 
+	use Airplane_Manage
+	select CHUYENBAY.MaCB, CHUYENBAY.GaDi, CHUYENBAY.GaDen
+	from CHUYENBAY, NHANVIEN, CHUNGNHAN, MAYBAY
+	where NHANVIEN.MaNV=CHUNGNHAN.MaNV and CHUNGNHAN.MaMB=MAYBAY.MaMB and MAYBAY.TamBay>=CHUYENBAY.DoDai
+	group by CHUYENBAY.MaCB, CHUYENBAY.GaDi, CHUYENBAY.GaDen
+	having min(NHANVIEN.Luong)>100000
+
+-- Bài 52. Cho biết tên các phi công chỉ lái các loại máy bay có tầm xa hơn 3200km và một trong số đó là Boeing 
+	use Airplane_Manage
+	select NHANVIEN.Ten
+	from NHANVIEN, CHUNGNHAN, MAYBAY
+	where NHANVIEN.MaNV=CHUNGNHAN.MaNV and CHUNGNHAN.MaMB=MAYBAY.MaMB and MAYBAY.TamBay > 3200 and MAYBAY.Loai like 'Boeing%'
+	group by NHANVIEN.Ten
+
+-- Bài 53. Tìm các phi công có thể lái tất cả các loại máy bay Boeing.
+	use Airplane_Manage
+	select NHANVIEN.MaNV, NHANVIEN.Ten
+	from NHANVIEN, CHUNGNHAN, MAYBAY
+	where NHANVIEN.MaNV=CHUNGNHAN.MaNV and CHUNGNHAN.MaMB=MAYBAY.MaMB and MAYBAY.Loai like 'Boeing%'
+	group by NHANVIEN.MaNV, NHANVIEN.Ten
+	having count(MAYBAY.MAMB)=(select count(MaMB) from MAYBAY where Loai like 'Boeing%')
